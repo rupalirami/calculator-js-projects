6fe84ba5e045271fa6ff3275f5ec6a2ed343307e
Rupali Anjaria-Rami <rupalianjaria-rami@Rupalis-iMac.home>
2022-09-22 23:14:40 -0400

    numbers 0,1,2 and operators *,-,+ working

--- calculator.js
+++ calculator.js
@@ -19,46 +19,218 @@
         // - can SQUARE ROOT
         // - can handle 3 or more numbers inputted
 
-// let firstNumber = Number(prompt("First number: "));
-// let operator = prompt("Operator: ");
-// let secondNumber = Number(prompt("Second number: "));
+// Store HTML elements in variables
 
-// alert(`${firstNumber} ${operator} ${secondNumber}`);
+// const operator = document.querySelectorAll(".opr");
+// const numArray = document.querySelectorAll(".num");
 
-// alert(`${firstNumber} is ${typeof firstNumber}`);
-// alert(`${secondNumber} is ${typeof secondNumber}`);
-// alert(`${operator} is ${typeof operator}`);
-// // alert(`length of operator ${operator.length}`);
+const numberZero = document.querySelector("#num-0");
+const numberOne = document.querySelector("#num-1");
+const numberTwo = document.querySelector("#num-2");
+// const numberThree = document.querySelector("#num-3");
+// const numberFour = document.querySelector("#num-4");
+// const numberFive = document.querySelector("#num-5");
+// const numberSix = document.querySelector("#num-6");
+// const numberSeven = document.querySelector("#num-7");
+// const numberEight = document.querySelector("#num-8");
+// const numberNine = document.querySelector("#num-9");
+
+
+// const oprPercentage = document.querySelector("#opr-percentage");
+// const oprDivide = document.querySelector("#opr-divide");
+const oprMultiply = document.querySelector("#opr-multiply");
+const oprSubstract = document.querySelector("#opr-substract");
+const oprAdd = document.querySelector("#opr-add");
+// const oprRoot = document.querySelector("#opr-root");
+// const actionBkSpace = document.querySelector("#opr-bkspace");
+// const actionCancel = document.querySelector("#opr-cancel");
+// const actionChangeSign = document.querySelector("#changesign");
+const actionCalculate = document.querySelector("#opr-calculate");
+
+const inputDisplay = document.querySelectorAll(".input-display");
+const resultsDisplay = document.querySelectorAll(".results-display");
+
+console.log("numbers 0,1,2 and operators *,-,+ working");
+// console.log(numberZero);
+// console.log(numberOne);
+// console.log(numberTwo);
+// console.log(numberThree);
+// console.log(numberFour);
+// console.log(numberFive);
+// console.log(numberSix);
+// console.log(numberSeven);
+// console.log(numberEight);
+// console.log(numberNine);
+// console.log(numArray);
+// console.log(oprPercentage);
+// console.log(oprDivide);
+// console.log(oprMultiply);
+// console.log(oprSubstract);
+// console.log(oprAdd);
+// console.log(oprRoot);
+// console.log(actionBkSpace);
+// console.log(actionCancel);
+// console.log(actionChangeSign);
+console.log(actionCalculate);
+// console.log(inputDisplay);
+// console.log(resultsDisplay);
+
+// initialize variables
+// let results = 0;
+const initializeParam = () => {
+    let firstInput = "";
+    let secondInput = "";
+    let inputDisplayText = "";
+    let resultsDisplayText = "";
+    let isOperatorClicked = false;
+    let operator = "";
+    // let changeSignDisplay = "";
+};
+// initializeParam();
+
+let firstInput = "";
+let secondInput = "";
+let inputDisplayText = "";
+let resultsDisplayText = "";
+let isOperatorClicked = false;
+let operator = "";
+// let changeSignDisplay = "";
+
+// calculateNow FUNCTION using two numbers and operator when opr-Calculate clicked and changes text for HTML classes input-display and results-display
 
 const calculateNow = (first, second, operation) => {
-    console.log(operation);
-    let errorMsg = "";
+    //change to return a variable and not explicit return
+    // let results;
+    // let errorMsg = 0;
+    // console.log(`first number is ${first}`);
+    // console.log(`first number is ${second}`);
+    console.log(`Operator is ${operation}`);
+    console.log(`typeof first input ${typeof first}`);
+    console.log(`typeof second input ${typeof second}`);
     switch (operation) {
         case "+":
-            return (first + second);
+            results = (first + second);
+            // console.log("in case addition " + results);
+            break;
         case "-":
-            return (first - second);
+            results = (first - second);
+            // console.log("in case multiplication");
+            break;
         case "/":
-            // if (second === 0) {
-            //     errorMsg = "Error: Division by 0 not permitted"
-            //     // clear secondNumberInput string variable and refresh display input
-            //     return;
-            // } else {
-            //     return first/second;
-            // }
-            // // remove ternary command to ensure errorMsg can be captured
-            return (second === 0) ? "Error: Division by 0 not permitted": (first / second) ;
-        case "*":
-            return (first * second);
-        case "%":
-            return (first*(second/100));
-        case "√":
-            if (second > 0 && second < 6) {
-                return Math.pow(first, 1/second).toFixed(2);
+            console.log("in case division");
+            if (second === 0) {
+                // console.log("in division by 0");
+                // errorMsg = 1;
+                results = "Error: Division by 0 not permitted"
+                // clear secondNumberInput string variable and refresh display input
+                return;
             } else {
-                return "Error: power can only be between 1 and 5 for now!";
-            }         
-    }
+                // console.log("in non-zero division")
+                results = first/second;
+            }
+            // remove ternary command to ensure errorMsg can be captured
+            // results = (second === 0) ? "Error: Division by 0 not permitted": (first / second) ;
+            break;
+        case "*":
+            // console.log("in case multiplication");
+            results = (first * second);
+            break;
+        case "%":
+            // console.log("in case percentage");
+            results = (first*(second/100));
+            break;
+        case "√":
+            // console.log("in case power")
+            if (second > 0 && second < 6) {
+                // console.log("in power if");
+                results = Math.pow(first, 1/second).toFixed(2);
+            } else {
+                // console.log("in power else")
+                // errorMsg = 1;
+                results = "Error: power can only be between 1 and 5 for now!";
+            };  
+            break;      
+    } 
+    return results;
 }
-let calcResults = calculateNow(firstNumber,secondNumber,operator)
-// alert(`The results of ${firstNumber} ${operator} ${secondNumber} are ${calcResults}`);
+
+// Get OPERATOR values
+oprAdd.addEventListener("click", () => {
+    // oprAdd.value;
+    operator = "+";
+    isOperatorClicked = true;
+    inputDisplayText = inputDisplayText + operator;
+        console.log(`opr variable on click is ${operator}`);
+});
+oprMultiply.addEventListener("click", () => {
+    operator = "*";
+    isOperatorClicked = true;
+    inputDisplayText = inputDisplayText + operator;
+        console.log(`opr variable on click is ${operator}`);
+});
+oprSubstract.addEventListener("click", () => {
+    operator = "-";
+    isOperatorClicked = true;
+    inputDisplayText = inputDisplayText + operator;
+        console.log(`opr variable on click is ${operator}`);
+});
+
+// Get NUMBER values
+numberZero.addEventListener("click", () => {
+    if (firstInput != "" && isOperatorClicked) {
+        secondInput = secondInput + "0";
+        inputDisplayText = inputDisplayText + secondInput;
+    } else {
+        firstInput = firstInput + "0";
+        inputDisplayText = inputDisplayText + firstInput;
+    }
+        console.log(`first input on click-1 is ${firstInput} data value is ${typeof firstInput}`);
+        console.log(`second input on click-1 is ${secondInput} data value ${typeof secondInput}`);
+    inputDisplay.innerHTML = inputDisplayText;
+});
+numberOne.addEventListener("click", () => {
+    if (firstInput != "" && isOperatorClicked) {
+        secondInput = secondInput + "1";
+        inputDisplayText = inputDisplayText + secondInput;
+    } else {
+        firstInput = firstInput + "1";
+        inputDisplayText = inputDisplayText + firstInput;
+    }
+        console.log(`first input on click-1 is ${firstInput} data value is ${typeof firstInput}`);
+        console.log(`second input on click-1 is ${secondInput} data value ${typeof secondInput}`);
+    inputDisplay.innerHTML = inputDisplayText;
+});
+numberTwo.addEventListener("click", () => {
+    if (firstInput != "" && isOperatorClicked) {
+        secondInput = secondInput + "2";
+        inputDisplayText = inputDisplayText + secondInput;
+    } else {
+        firstInput = firstInput + "2";
+        inputDisplayText = inputDisplayText + firstInput;
+    }
+        console.log(`first input on click-1 is ${firstInput} data value is ${typeof firstInput}`);
+        console.log(`second input on click-1 is ${secondInput} data value ${typeof secondInput}`);
+    inputDisplay.innerHTML = inputDisplayText;
+});
+
+// numArray.forEach((num) => {
+//     num.addEventListener("click" , () => num.append());
+// });
+
+// Calculate RESULTS on = click; initialize parameters
+actionCalculate.addEventListener("click",function() {
+        console.log(`input values are num1 ${firstInput} (${typeof firstInput}), opr ${operator}, num2 ${secondInput} (${typeof secondInput})`);
+    const firstInputNumber = Number(firstInput);
+    const secondInputNumber = Number(secondInput);
+    resultsDisplayText=calculateNow(firstInputNumber,secondInputNumber,operator);
+        console.log(`results = ${resultsDisplayText}`);
+
+    firstInput = "";
+    secondInput = "";
+    inputDisplayText = "";
+    resultsDisplayText = "";
+    isOperatorClicked = false;
+    operator = "";
+    // changeSignDisplay = "";
+});
+
